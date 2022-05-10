93b14d08ca8e066c7d7d8d22970364519a668012
Daniel Molina <danmnvx@gmail.com>
2022-05-10 14:05:47 -0500

    Themes setup

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -4,4 +4,20 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: { 
+        dark: true,
+        options: { customProperties: true },
+        themes: {
+            light: {
+                primary: '#0b5da6',
+                secondary: '#ff823b',
+                accent: '#6f8c9c',
+            },
+            dark: {
+                primary: '#0b5da6',
+                secondary: '#ff823b',
+                accent: '#6f8c9c',
+            }
+        }
+    },
 });
