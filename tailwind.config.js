03f5b2b35dd25ec36038d423de5a3bec5f97b5c3
Daniel Molina <danmnvx@gmail.com>
2022-10-14 11:09:38 -0500

    New structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
@@ -11,6 +11,7 @@ module.exports = {
     require("daisyui")
   ],
   daisyui: {
-    themes: ["cupcake", "garden", "emerald", "winter"]
+    themes: ["cupcake", "garden", "emerald", "winter"],
+    prefix: "ds-"
   }
 }
