63e231401ee6b6bea1a74945453d809e401e8407
Daniel Molina <danmnvx@gmail.com>
2022-10-30 16:28:45 -0600

    Layout structure (#3)
    Added icons and some styles
    
    Added structure of layout
    
    Workflow of PR updated

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,12 +1,14 @@
 
 import Navbar from './navbar'
+import Footer from './footer'
 
 export default function Layout({ children }) {
     return (
         <>
-            <Navbar />
-            <main>{children}</main>
-            {/* <Footer /> */}
+            <Navbar>
+                <main>{children}</main>
+            </Navbar>
+            <Footer />
         </>
     )
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,46 +1,37 @@
-export default function Layout({ }) {
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+export default function Navbar({ children }) {
     return (
-        <div className="navbar bg-base-100">
-            <div className="navbar-start">
-                <div className="dropdown">
-                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                    </label>
-                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a>Item 1</a></li>
-                        <li tabIndex={0}>
-                            <a className="justify-between">
-                                Parent
-                                <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
-                            </a>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><a>Item 3</a></li>
-                    </ul>
-                </div>
-                <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
-            </div>
-            <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal p-0">
-                    <li><a>Item 1</a></li>
-                    <li tabIndex={0}>
-                        <a>
-                            Parent
-                            <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
-                        </a>
-                        <ul className="p-2">
-                            <li><a>Submenu 1</a></li>
-                            <li><a>Submenu 2</a></li>
+        <div className="ds-drawer">
+            <input id="my-drawer-3" type="checkbox" className="ds-drawer-toggle" />
+            <div className="ds-drawer-content flex flex-col">
+                <div className="w-full ds-navbar bg-base-300">
+                    <div className="flex-none lg:hidden">
+                        <label htmlFor="my-drawer-3" className="ds-btn ds-btn-square ds-btn-ghost">
+                            <FontAwesomeIcon icon={faBars} size="xl" />
+                        </label>
+                    </div>
+                    <div className="flex-1 px-2 mx-2">
+                        <img className="h-16 w-auto" src="/img/logo.png" />
+                    </div>
+                    <div className="flex-none hidden lg:block">
+                        <ul className="ds-menu ds-menu-horizontal">
+                            <li><a>Navbar Item 1</a></li>
+                            <li><a>Navbar Item 2</a></li>
                         </ul>
-                    </li>
-                    <li><a>Item 3</a></li>
-                </ul>
+                    </div>
+                </div>
+                { /* Main content */}
+                {children}
             </div>
-            <div className="navbar-end">
-                <a className="btn">Get started</a>
+
+            <div className="ds-drawer-side">
+                <label htmlFor="my-drawer-3" className="ds-drawer-overlay"></label>
+                <ul className="ds-menu p-4 overflow-y-auto w-80 bg-base-100">
+                    <li><a>Sidebar Item 1</a></li>
+                    <li><a>Sidebar Item 2</a></li>
+                </ul>
             </div>
         </div>
     )
